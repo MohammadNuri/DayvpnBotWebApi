d8298a680a67bf260e4f228912b751e62d2284df
Nouri <Nouri@NOURI>
2025-07-28 07:19:46 +0330

    Init Admin Panel +React +Vite + MUI

--- dayvpnadminpanel/src/App.tsx
+++ dayvpnadminpanel/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import rtlPlugin from 'stylis-plugin-rtl';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import { prefixer } from 'stylis';
+import theme from './theme';
+import Router from './routes';
 
-function App() {
-  const [count, setCount] = useState(0)
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
-
-export default App
---
+++ dayvpnadminpanel/src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import { Typography, Container } from '@mui/material';
+
+export default function NotFound() {
+  return (
+    <Container>
+      <Typography variant="h5">صفحه مورد نظر پیدا نشد</Typography>
+    </Container>
+  );
+}
